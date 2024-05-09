75f2575709dfb8fc5c4507147ee22148f97de9e1
amandeep singh parihar <amandeepparihar24@gmail.com>
2024-05-09 23:20:23 +0530

    adding cursor and page1 part 1

--- script.js
+++ script.js
@@ -40,4 +40,18 @@ function loadingAnimation() {
     display: "none",
   });
 }
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4");
+}
+
 loadingAnimation();
+
+cursorAnimation();
